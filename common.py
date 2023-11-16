68aae7446fec764d3bc82aab3377fc6b71e3089e
Biel-Hammer <creepui@gmail.com>
2023-11-16 20:48:55 +0800

    wonder3d_utils

---
+++ common.py
@@ -0,0 +1,108 @@
+import cv2
+from typing import Dict, Optional, Tuple, List
+from dataclasses import dataclass
+from utils.misc import load_config
+from omegaconf import OmegaConf
+import torch
+from PIL import Image
+import numpy as np
+import os
+from mvdiffusion.data.single_image_dataset import SingleImageDataset
+
+
+@dataclass
+class TestConfig:
+    pretrained_model_name_or_path: str
+    pretrained_unet_path: str
+    revision: Optional[str]
+    validation_dataset: Dict
+    save_dir: str
+    seed: Optional[int]
+    validation_batch_size: int
+    dataloader_num_workers: int
+
+    local_rank: int
+
+    pipe_kwargs: Dict
+    pipe_validation_kwargs: Dict
+    unet_from_pretrained_kwargs: Dict
+    validation_guidance_scales: List[float]
+    validation_grid_nrow: int
+    camera_embedding_lr_mult: float
+
+    num_views: int
+    camera_embedding_type: str
+
+    pred_type: str  # joint, or ablation
+
+    enable_xformers_memory_efficient_attention: bool
+
+    cond_on_normals: bool
+    cond_on_colors: bool
+
+
+def get_config():
+    # parse YAML config to OmegaConf
+    cfg = load_config("./configs/mvdiffusion-joint-ortho-6views.yaml")
+    # print(cfg)
+    schema = OmegaConf.structured(TestConfig)
+    cfg = OmegaConf.merge(schema, cfg)
+    return cfg
+
+
+def preprocess_data(single_image, crop_size):
+    dataset = SingleImageDataset(
+        root_dir="",
+        num_views=6,
+        img_wh=[256, 256],
+        bg_color="white",
+        crop_size=crop_size,
+        single_image=single_image,
+    )
+    return dataset[0]
+
+
+def save_image(tensor):
+    ndarr = (
+        tensor.mul(255)
+        .add_(0.5)
+        .clamp_(0, 255)
+        .permute(1, 2, 0)
+        .to("cpu", torch.uint8)
+        .numpy()
+    )
+    im = Image.fromarray(ndarr)
+    return ndarr
+
+
+def save_image_to_disk(tensor, fp):
+    ndarr = (
+        tensor.mul(255)
+        .add_(0.5)
+        .clamp_(0, 255)
+        .permute(1, 2, 0)
+        .to("cpu", torch.uint8)
+        .numpy()
+    )
+    im = Image.fromarray(ndarr)
+    im.save(fp)
+    return ndarr
+
+
+def save_image_numpy(ndarr, fp):
+    im = Image.fromarray(ndarr)
+    im.save(fp)
+
+
+def expand2square(pil_img, background_color):
+    width, height = pil_img.size
+    if width == height:
+        return pil_img
+    elif width > height:
+        result = Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+        return result
+    else:
+        result = Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+        return result
---
+++ wonder3d_utils.py
@@ -0,0 +1,237 @@
+import os
+import torch
+from rembg import remove
+import torch.nn as nn
+from einops import rearrange
+from PIL import Image
+from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
+from mvdiffusion.models.unet_mv2d_condition import UNetMV2DConditionModel
+from mvdiffusion.data.single_image_dataset import (
+    SingleImageDataset as MVDiffusionDataset,
+)
+from mvdiffusion.pipelines.pipeline_mvdiffusion_image import MVDiffusionImagePipeline
+from diffusers import AutoencoderKL, DDPMScheduler, DDIMScheduler
+from common import (
+    get_config,
+    save_image,
+    save_image_numpy,
+    save_image_to_disk,
+    preprocess_data,
+    expand2square,
+)
+
+
+class Wonder3d(nn.Module):
+    def __init__(
+        self,
+        device,
+    ):
+        super(Wonder3d, self).__init__()
+        self.device = device
+        self.fp16 = torch.float16
+        self.cfg = get_config()
+        self.pipe = self.init_pipeline()
+        torch.set_grad_enabled(False)
+        self.pipe.to(self.device)
+
+    def init_pipeline(self):
+        """
+        Initializes and returns a pipeline object for multi-modal diffusion image processing.
+
+        Args:
+            cfg (Config): The configuration object containing all the necessary parameters.
+            weight_dtype (torch.dtype): The data type to which the weights of the models should be cast.
+
+        Returns:
+            MVDiffusionImagePipeline: The pipeline object for multi-modal diffusion image processing.
+        """
+        image_encoder = CLIPVisionModelWithProjection.from_pretrained(
+            self.cfg.pretrained_model_name_or_path,
+            subfolder="image_encoder",
+            revision=self.cfg.revision,
+        )
+        feature_extractor = CLIPImageProcessor.from_pretrained(
+            self.cfg.pretrained_model_name_or_path,
+            subfolder="feature_extractor",
+            revision=self.cfg.revision,
+        )
+        vae = AutoencoderKL.from_pretrained(
+            self.cfg.pretrained_model_name_or_path,
+            subfolder="vae",
+            revision=self.cfg.revision,
+        )
+        unet = UNetMV2DConditionModel.from_pretrained_2d(
+            self.cfg.pretrained_unet_path,
+            subfolder="unet",
+            revision=self.cfg.revision,
+            **self.cfg.unet_from_pretrained_kwargs,
+        )
+        unet.enable_xformers_memory_efficient_attention()
+
+        # Move text_encode and vae to gpu and cast to weight_dtype
+        image_encoder.to(dtype=self.fp16)
+        vae.to(dtype=self.fp16)
+        unet.to(dtype=self.fp16)
+
+        pipeline = MVDiffusionImagePipeline(
+            image_encoder=image_encoder,
+            feature_extractor=feature_extractor,
+            vae=vae,
+            unet=unet,
+            safety_checker=None,
+            scheduler=DDIMScheduler.from_pretrained(
+                self.cfg.pretrained_model_name_or_path, subfolder="scheduler"
+            ),
+            **self.cfg.pipe_kwargs,
+        )
+        return pipeline
+
+    def run_pipeline(
+        self,
+        single_image,
+        guidance_scale,
+        steps,
+        seed,
+        crop_size,
+        write_image=False,
+    ):
+        """
+        Runs the pipeline to process a single image.
+
+        Args:
+            single_image (Tensor): The input image to be processed.
+            guidance_scale (float): The scale factor for the guidance.
+            steps (int): The number of inference steps to perform.
+            seed (int): The seed for random number generation.
+            crop_size (int): The size of the image crop.
+            chk_group (Optional[str]): The checkpoint group.
+
+        Returns:
+            List[Image]: A list of processed images.
+        """
+        # if chk_group is not None:
+        #     write_image = "Write Results" in chk_group
+
+        batch = preprocess_data(single_image, crop_size)
+
+        self.pipe.set_progress_bar_config(disable=True)
+        seed = int(seed)
+        generator = torch.Generator(device=self.pipe.unet.device).manual_seed(seed)
+
+        # repeat  (2B, Nv, 3, H, W)
+        imgs_in = torch.cat([batch["imgs_in"]] * 2, dim=0).to(self.fp16)
+
+        # (2B, Nv, Nce)
+        camera_embeddings = torch.cat([batch["camera_embeddings"]] * 2, dim=0).to(
+            self.fp16
+        )
+
+        task_embeddings = torch.cat(
+            [batch["normal_task_embeddings"], batch["color_task_embeddings"]], dim=0
+        ).to(self.fp16)
+
+        camera_embeddings = torch.cat([camera_embeddings, task_embeddings], dim=-1).to(
+            self.fp16
+        )
+
+        # (B*Nv, 3, H, W)
+        imgs_in = rearrange(imgs_in, "Nv C H W -> (Nv) C H W")
+        # (B*Nv, Nce)
+        # camera_embeddings = rearrange(camera_embeddings, "B Nv Nce -> (B Nv) Nce")
+
+        out = self.pipe(
+            imgs_in,
+            camera_embeddings,
+            generator=generator,
+            guidance_scale=guidance_scale,
+            num_inference_steps=steps,
+            output_type="pt",
+            num_images_per_prompt=1,
+            **self.cfg.pipe_validation_kwargs,
+        ).images
+
+        bsz = out.shape[0] // 2
+        normals_pred = out[:bsz]
+        images_pred = out[bsz:]
+        num_views = 6
+        if write_image:
+            VIEWS = ["front", "front_right", "right", "back", "left", "front_left"]
+            cur_dir = os.path.join(
+                "./outputs", f"cropsize-{crop_size}-cfg{guidance_scale:.1f}"
+            )
+
+            scene = "scene"
+            scene_dir = os.path.join(cur_dir, scene)
+            normal_dir = os.path.join(scene_dir, "normals")
+            masked_colors_dir = os.path.join(scene_dir, "masked_colors")
+            os.makedirs(normal_dir, exist_ok=True)
+            os.makedirs(masked_colors_dir, exist_ok=True)
+            for j in range(num_views):
+                view = VIEWS[j]
+                normal = normals_pred[j]
+                color = images_pred[j]
+
+                normal_filename = f"normals_000_{view}.png"
+                rgb_filename = f"rgb_000_{view}.png"
+                normal = save_image_to_disk(
+                    normal, os.path.join(normal_dir, normal_filename)
+                )
+                color = save_image_to_disk(color, os.path.join(scene_dir, rgb_filename))
+
+                rm_normal = remove(normal)
+                rm_color = remove(color)
+
+                save_image_numpy(rm_normal, os.path.join(scene_dir, normal_filename))
+                save_image_numpy(
+                    rm_color, os.path.join(masked_colors_dir, rgb_filename)
+                )
+
+        normals_pred = [save_image(normals_pred[i]) for i in range(bsz)]
+        images_pred = [save_image(images_pred[i]) for i in range(bsz)]
+
+        out = images_pred + normals_pred
+        return out
+
+
+if __name__ == "__main__":
+    import argparse
+    import matplotlib.pyplot as plt
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=str, help="input image path")
+    parser.add_argument(
+        "--guidance_scale",
+        type=int,
+        default=3,
+        help="Classifier Free Guidance Scale, 1-5",
+    )
+    parser.add_argument(
+        "--steps",
+        type=int,
+        default=50,
+        help="Number of Diffusion Inference Steps,15-100",
+    )
+    parser.add_argument("--seed", type=int, default=42, help="Seed")
+    parser.add_argument("--crop_size", type=int, default=224, help="Crop Size")
+    parser.add_argument("--device", default="cuda", help="Device (cuda or cpu)")
+
+    opt = parser.parse_args()
+
+    print(f"[INFO] running model ...")
+    wonder3d = Wonder3d(opt.device)
+    input_image = Image.open(opt.input)
+    input_image = expand2square(input_image, (127, 127, 127, 0))
+    print(f"[INFO] loading image from {opt.input} ...")
+    outputs = wonder3d.run_pipeline(
+        input_image,
+        opt.guidance_scale,
+        opt.steps,
+        opt.seed,
+        opt.crop_size,
+        write_image=True,
+    )
+    torch.cuda.empty_cache()
+    plt.figure(len(outputs))
+    for output in outputs:
+        plt.imshow(output)
+    plt.show()
